8dbf9436bc982527d4db3cce65ebc4dbe13aec3a
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-12-13 18:06:13 +0100

    added en-us language

---
+++ src/catalog/language/en-us/captcha/ps_google_recaptcha.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_captcha']                 = 'Captcha';
+$_['text_recaptcha_branding']      = 'This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.';
+
+// Entry
+$_['entry_challange_captcha']      = 'Please complete the captcha validation below';
+$_['entry_score_based_captcha']    = 'Captcha validation';
+
+// Error
+$_['error_captcha']                = 'Verification is not correct!';
+$_['error_missing_input_secret']   = 'The secret parameter is missing.';
+$_['error_invalid_input_secret']   = 'The secret parameter is invalid or malformed.';
+$_['error_missing_input_response'] = 'The response parameter is missing.';
+$_['error_invalid_input_response'] = 'The response parameter is invalid or malformed.';
+$_['error_bad_request']            = 'The request is invalid or malformed.';
+$_['error_timeout_or_duplicate']   = 'The response is no longer valid: either is too old or has been used previously.';
