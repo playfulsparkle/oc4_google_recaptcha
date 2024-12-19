42ad86c6e71c606c8db00661f3df90865fee49f9
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-12-19 17:52:24 +0100

    catalog login bug

--- src/catalog/controller/captcha/ps_google_recaptcha.php
+++ src/catalog/controller/captcha/ps_google_recaptcha.php
@@ -302,6 +302,25 @@ class PsGoogleReCaptcha extends \Opencart\System\Engine\Controller
 			$captcha = $this->load->controller('extension/' . $extension_info['extension'] . '/captcha/' . $extension_info['code'] . '.validate');
 
 			if ($captcha) {
+                if (isset($json['redirect']) && $this->customer->isLogged()) {
+                    $this->customer->logout();
+
+                    unset($json['redirect']);
+
+                    unset($this->session->data['customer']);
+                    unset($this->session->data['shipping_address']);
+                    unset($this->session->data['shipping_method']);
+                    unset($this->session->data['shipping_methods']);
+                    unset($this->session->data['payment_address']);
+                    unset($this->session->data['payment_method']);
+                    unset($this->session->data['payment_methods']);
+                    unset($this->session->data['comment']);
+                    unset($this->session->data['order_id']);
+                    unset($this->session->data['coupon']);
+                    unset($this->session->data['reward']);
+                    unset($this->session->data['customer_token']);
+                }
+
 				$json['error']['captcha'] = $captcha;
 			}
 		}
